976cdfc4da65100ea5f90b658e6eab6573dc1fe6
lexik7 <113137861+lexik7@users.noreply.github.com>
2022-12-12 22:40:43 -0800

    enhypen 3-page site

---
+++ what-you-know/js/script.js
@@ -0,0 +1,41 @@
+function won() {
+    let Wonparagraph = document.getElementById('won')
+    Wonparagraph.innerHTML = 'Jungwon is one of the members that snores the most!';
+    Wonparagraph.style.display = 'block'
+}
+
+function hee() {
+    let Heeparagraph = document.getElementById('hee')
+    Heeparagraph.innerHTML = 'Heeseung’s favorite colors are purple and ivory!';
+    Heeparagraph.style.display = 'block'
+}
+
+function jay() {
+    let Jayparagraph = document.getElementById('jay')
+    Jayparagraph.innerHTML = 'Jay was born in Seattle, Washington!';
+    Jayparagraph.style.display = 'block'
+}
+
+function jake() {
+    let Jakeparagraph = document.getElementById('jake')
+    Jakeparagraph.innerHTML = 'Jake has a dog named Layla!';
+    Jakeparagraph.style.display = 'block'
+}
+
+function hoon() {
+    let Hoonparagraph = document.getElementById('hoon')
+    Hoonparagraph.innerHTML = 'Sunghoon used to be a competitive ice skater!';
+    Hoonparagraph.style.display = 'block'
+}
+
+function sunoo() {
+    let Sunooparagraph = document.getElementById('sunoo')
+    Sunooparagraph.innerHTML = 'Sunoo  has a black belt in hapkido!';
+    Sunooparagraph.style.display = 'block'
+}
+
+function riki() {
+    let Rikiparagraph = document.getElementById('riki')
+    Rikiparagraph.innerHTML = 'Ni-Ki is left handed and ambidextrious!';
+    Rikiparagraph.style.display = 'block'
+}
